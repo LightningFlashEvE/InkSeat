464e4b215d50ac501833703d86d640d9f109c2cb
嘉浩 周 <LightningFlashEVE@outlook.com>
2026-08-06 18:27:54 +0800

    Move device publish action to panel footer

--- cloud_server/frontend/devices.js
+++ cloud_server/frontend/devices.js
@@ -533,7 +533,7 @@ function setSignalIndicator(id, rssi) {
 }
 
 function openDevice(deviceId) {
-    window.location.href = `control.html?v=20260806device2&view=device-editor&deviceId=${encodeURIComponent(deviceId)}`;
+    window.location.href = `control.html?v=20260806device3&view=device-editor&deviceId=${encodeURIComponent(deviceId)}`;
 }
 
 let pollingInterval = null;
