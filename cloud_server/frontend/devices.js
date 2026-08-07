51f6cb1117e2f26c9a37f1dbf992a53d0c9750f3
嘉浩 周 <LightningFlashEVE@outlook.com>
2026-08-07 11:31:36 +0800

    Move canvas status below editor

--- cloud_server/frontend/devices.js
+++ cloud_server/frontend/devices.js
@@ -533,7 +533,7 @@ function setSignalIndicator(id, rssi) {
 }
 
 function openDevice(deviceId) {
-    window.location.href = `control.html?v=20260807logotools1&view=device-editor&deviceId=${encodeURIComponent(deviceId)}`;
+    window.location.href = `control.html?v=20260807canvasbadge1&view=device-editor&deviceId=${encodeURIComponent(deviceId)}`;
 }
 
 let pollingInterval = null;
